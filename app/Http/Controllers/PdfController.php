01565c62172baa90d3e69b76c7dac7ba3a2fd9ba
franciscooliver <frandosax@gmail.com>
2018-09-01 14:47:52 -0300

    test_gitignore

--- app/Http/Controllers/PdfController.php
+++ app/Http/Controllers/PdfController.php
@@ -24,11 +24,11 @@ class PdfController extends Controller
         $array_linhaG = $this->geraLinha(46,60,5);
         $array_linhaO = $this->geraLinha(61,75,5);
 
-        $B = array($array_linhaB[0],$array_linhaI[0],$array_linhaN[0],$array_linhaN[0],$array_linhaO[0]);
-        $I = array($array_linhaB[1],$array_linhaI[1],$array_linhaN[1],$array_linhaN[1],$array_linhaO[1]);
-        $N = array($array_linhaB[2],$array_linhaI[2],$array_linhaN[2],$array_linhaN[2],$array_linhaO[2]);
-        $G = array($array_linhaB[3],$array_linhaI[3],$array_linhaN[3],$array_linhaN[3],$array_linhaO[3]);
-        $O = array($array_linhaB[4],$array_linhaI[4],$array_linhaN[4],$array_linhaN[4],$array_linhaO[4]);
+        $B = array($array_linhaB[0],$array_linhaI[0],$array_linhaN[0],$array_linhaG[0],$array_linhaO[0]);
+        $I = array($array_linhaB[1],$array_linhaI[1],$array_linhaN[1],$array_linhaG[1],$array_linhaO[1]);
+        $N = array($array_linhaB[2],$array_linhaI[2],$array_linhaN[2],$array_linhaG[2],$array_linhaO[2]);
+        $G = array($array_linhaB[3],$array_linhaI[3],$array_linhaN[3],$array_linhaG[3],$array_linhaO[3]);
+        $O = array($array_linhaB[4],$array_linhaI[4],$array_linhaN[4],$array_linhaG[4],$array_linhaO[4]);
 
         $N[2] ='--';
         //echo json_encode($N);
@@ -67,11 +67,15 @@ class PdfController extends Controller
         return $pdf->stream('cartela'.$numerocartela1.'-'.$numerocartela2.'.pdf');
 
     }
-
+    /*funcao recebe o valor minimo e maximo e a quantidade de numeros a ser gerados e retorna
+    * um array com os numeros randomicos
+    */
     public function geraLinha($min,$max,$count): Array{
-        $nums = range($min,$max);
+        $nums = range($min, $max);
+        //embaralha os valores
         shuffle($nums);
 
+        //array que seŕá retornado com os números
         $response = array();
         for($i=0; $i < $count;$i++){
             array_push($response, $nums[$i]);
---
+++ app/classes/CartelaInterface.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: fran_oliver
+ * Date: 01/09/18
+ * Time: 14:27
+ */
+
+namespace App\classes;
+
+
+interface CartelaInterface
+{
+    public function gerarCartela();
+    public function gerarLinha();
+}
